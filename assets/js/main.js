deb9ff811b92a502ae82a817e35fba18f07dccbe
colinmichael89 <colinmichael89@gmail.com>
2022-10-08 23:20:40 -0600

    added delete function, fixed save to local storage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,53 +1,81 @@
-// Define Variables
-// Selectors
-var currentDay = moment().format("ddd MMM Do, YYYY");
-$("#currentDay").text(currentDay);
-
-var appointmentAddedEl = $(`#appointment-added`);
-var hourEl = $(`.hour`);
-var eventDescriptionEl = $(`.event-description`);
-var saveButtonEl = $(`.saveBtn`);
-
-// Event Listeners
-
 $(document).ready(function () {
-  saveButtonEl.on(`click`, function () {
-    localStorage.setItem(
-      $(this).siblings(".col-1").attr("id"),
-      $(this).siblings(".event-description").val()
-    );
+  var currentDay = moment().format("ddd MMM Do, YYYY");
+  $("#currentDay").text(currentDay);
+
+  // Event Listener
+  $(".saveBtn").on("click", function () {
+    // Get values
+    var event = $(this).siblings(".event-description").val();
+    var time = $(this).parent().attr("id");
+
+    // Save to local storage
+    localStorage.setItem(time, event);
+
+    // Notification event/time was stored in local storage
+    $(".notification").addClass("show");
+
+    setTimeout(function () {
+      // Hide notification
+      $(".notification").removeClass("show");
+    }, 3000);
+
+    // Delete event from local storage
+    $(".deleteBtn").on("click", function () {
+      localStorage.removeItem(time, event);
+      // Clear event text
+      $(".event-description").val("");
+
+      // Notification to show event was deleted
+      $(".delete").addClass("show");
+
+      setTimeout(function () {
+        // Hide notification
+        $(".delete").removeClass("show");
+      }, 3000);
+    });
   });
 
-  // Logic // Call Functions
-  var currentHour = moment().format("H");
+  function updateHour() {
+    var currentHour = moment().hours();
+    // loop over time blocks
+    $(".time").each(function () {
+      var hourBlock = parseInt($(this).attr("id").split("-")[1]);
 
-  var hourArray = $(".hour");
-
-  hourArray
-    .each((element) => {
-      console.log(hourArray[element].id);
-      console.log(currentHour);
-
-      $(hourArray[element].id) === currentHour;
-    })
-    .addClass("future");
-
-  hourArray
-    .each((element) => {
-      $(hourArray[element].id) > currentHour;
-    })
-    .addClass("future");
-
-  hourArray
-    .each((element) => {
-      $(hourArray[element].id) < currentHour;
-    })
-    .addClass("past");
-});
-
-function allStorage() {
-  var archive = [];
-  for (var i = 0; i < localStorage.length; i++) {
-    archive[i] = localStorage.getItem(localStorage.key(i));
+      // Color code hour blocks
+      if (hourBlock < currentHour) {
+        $(this).addClass("past");
+      } else if (blockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
   }
-}
+
+  updateHour();
+
+  // set up interval to check if current time needs to be updated
+  var interval = setInterval(updateHour, 15000);
+
+  // Get data from local storage
+  $("#hour-9 .event-description").val(localStorage.getItem("hour-9"));
+
+  $("#hour-10 .event-description").val(localStorage.getItem("hour-10"));
+
+  $("#hour-11 .event-description").val(localStorage.getItem("hour-11"));
+
+  $("#hour-12 .event-description").val(localStorage.getItem("hour-12"));
+
+  $("#hour-13 .event-description").val(localStorage.getItem("hour-13"));
+
+  $("#hour-14 .event-description").val(localStorage.getItem("hour-14"));
+
+  $("#hour-15 .event-description").val(localStorage.getItem("hour-15"));
+
+  $("#hour-16 .event-description").val(localStorage.getItem("hour-16"));
+
+  $("#hour-17 .event-description").val(localStorage.getItem("hour-17"));
+});
